32f2fa8e236bd51994a627d7340930bc9aad5e98
Seth Berry <seth.berry@nd.edu>
2017-09-28 21:41:51 -0400

    Including R code file and general updates (mostly gifs).

---
+++ dataWrangling2017.R
@@ -0,0 +1,167 @@
+#####################
+### Workshop Code ###
+#####################
+
+### Package Installation ###
+
+# The easiest way to run a line of code is to put your cursor
+# anywhere on the line and press Control + Enter.
+
+install.packages(c('tidyverse', 'plotly', 'DT', 'data.table'))
+
+# You will only ever need to run the previous line once (unless you update R).
+
+### The Basics ###
+
+numList = 1:5
+
+numList
+
+### The Functions ###
+
+nonsenseFunction = function(x) {
+  res = (sqrt(x) + sin(x))^2
+  return(res)
+}
+
+test = 1:10
+
+nonsenseFunction(test)
+
+### Our Data ###
+
+library(rvest); library(xml2); library(magrittr)
+
+filmLink = "https://en.wikipedia.org/wiki/List_of_highest-grossing_films"
+
+allTables = read_html(filmLink) %>%
+  html_table(fill = TRUE)
+
+highestGrossing = allTables %>%
+  extract2(1)
+
+head(highestGrossing)
+
+# You may want to see all of your data -- use View(highestGrossing)
+
+### Action! (Or using select) ###
+
+highestGrossing = highestGrossing %>%
+  select(-Rank, -`Reference(s)`)
+
+head(highestGrossing)
+
+### String Cleaning ###
+
+library(stringr)
+
+highestGrossing = highestGrossing %>%
+  mutate(Peak = str_replace(Peak, "[A-Z].*", ""),
+         Peak = as.numeric(Peak),
+         gross = as.numeric(str_replace_all(`Worldwide gross`, "\\$|,", ""))) %>%
+  select(-`Worldwide gross`)
+
+### Filter ###
+
+highestGrossing = highestGrossing %>%
+  filter(Peak != 1 & Peak != max(Peak))
+
+### GDP Deflator ###
+
+gdpDeflator2017 = read_html("http://www.multpl.com/gdp-deflator/table") %>%
+  html_table(trim = TRUE, header = TRUE) %>%
+  extract2(1) %>%
+  filter(grepl("2017", Date) == TRUE) %>%
+  select(-Date) %>%
+  extract2(1)
+
+### Using Mutate ###
+
+highestGrossing = highestGrossing %>%
+  mutate(grossReal2017 = gross/(gdpDeflator2017/100))
+
+### Group_by ###
+
+highestGrossing = highestGrossing %>%
+  group_by(Year)
+
+### Summarize ###
+
+highestGrossingSummary = highestGrossing %>%
+  summarize(n = n(),
+            meanGross = mean(grossReal2017))
+
+head(highestGrossingSummary)
+
+### Coding How We Think ###
+
+allTables = read_html(filmLink) %>%
+  html_table(fill = TRUE) %>%
+  extract2(1) %>%
+  select(-Rank, -`Reference(s)`) %>%
+  mutate(Peak = str_replace(Peak, "[A-Z].*", ""),
+         Peak = as.numeric(Peak),
+         gross = as.numeric(str_replace_all(`Worldwide gross`, "\\$|,", ""))) %>%
+  select(-`Worldwide gross`) %>%
+  filter(Peak != 1 & Peak != max(Peak)) %>%
+  mutate(grossReal2017 = gross/(gdpDeflator2017/100)) %>%
+  group_by(Year) %>%
+  summarize(n = n(),
+            meanGross = mean(grossReal2017))
+
+
+# Are you still here, after all this time?
+# I am proud of you! Not only am I proud of
+# you, but so is Bill "The Wizard" Venables.
+# If you have made it here, without tears,
+# speak now (yes, right now -- I am probably
+# not saying anything that important) to
+# get your pick of R t-shirts. Be fast, though,
+# because it will only go to one person.
+
+### An Interactive Look ###
+
+library(DT)
+
+datatable(highestGrossingSummary)
+
+### Visual Exploration ###
+
+library(ggplot2)
+
+ggplot(highestGrossingSummary, aes(Year, meanGross, size = n)) +
+  geom_point() +
+  theme_minimal()
+
+### Something A Bit More Modern ###
+
+movieByYear = highestGrossing %>%
+  group_by(Year) %>%
+  dplyr::summarise(movies = paste(Title, collapse = "\n")) %>%
+  right_join(., highestGrossingSummary, by = "Year")
+
+library(plotly)
+
+p = ggplot(movieByYear, aes(Year, meanGross, size = n, color = meanGross)) +
+  geom_point(aes(text = movies)) +
+  theme_minimal() +
+  theme(axis.text.y=element_blank(),
+        axis.title.y=element_blank())
+
+ggplotly(p, tooltip = c("text", "size", "color"))
+
+### Coalesce ###
+
+testDF = data.frame(x1 = c(1:5, rep(NA, 5)),
+                    x2 = c(rep(NA, 5), 6:10))
+
+testDF
+
+testDF %>%
+  mutate(z = coalesce(x1, x2))
+
+# The first person to yell their favorite
+# dplyr function gets to pick the next shirt.
+# If you start a slow-clap/chant, I will throw
+# in a pick from the candy bag; someone should
+# join me in looking foolish.
